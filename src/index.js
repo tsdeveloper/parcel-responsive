e303283e552fd77f48d4f39e6fc93fdabcdff9d7
Developer F+ <desenvolvimento@fazendomais.com.br>
2019-07-12 11:39:59 -0300

    create responsive scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+// import '@fortawesome/fontawesome-free/css/fontawesome.css'
+// import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '@fontawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fontawesome/fontawesome-free/scss/solid.scss';
+import '@fontawesome/fontawesome-free/scss/brands.scss';
 import './scss/app.scss';
 import jquery from 'jquery';
 
